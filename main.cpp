7f93449d20201d41eea28e2b7142a7aabd99d4a2
laefad <FreeFlightA@yandex.ru>
2021-08-07 14:02:03 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <fmt/color.h>
+
+#include <string>
+
+#include <locale>
+#include <codecvt>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+std::wstring readFile(const char* filename)
+{
+    std::wifstream wif(filename);
+    wif.imbue(locale(wif.getloc(), new codecvt_utf16<wchar_t, 0x10ffff>()));
+    std::wstringstream wss;
+    wss << wif.rdbuf();
+    return wss.str();
+}
+
+
+void writeToFile(const char* filename, std::wstring& wstr)
+{
+    std::wofstream wof(filename);
+    wof.imbue(locale(wof.getloc(), new codecvt_utf16<wchar_t, 0x10ffff>()));
+    wof << wstr;
+}
+
+int main () {
+
+    setlocale(LC_ALL, "");
+
+    wstring wstr = readFile("./test.txt");
+    wcout << wstr << endl;
+    writeToFile("./result.txt", wstr);
+    
+
+
+    fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+               "Hello, {}!\n", "world");
+    fmt::print(fg(fmt::color::floral_white) | bg(fmt::color::slate_gray) |
+               fmt::emphasis::underline, "Hello, {}!\n", "мир");
+    fmt::print(fg(fmt::color::steel_blue) | fmt::emphasis::italic,
+               "Hello, {}!\n", "世界");
+               
+}
